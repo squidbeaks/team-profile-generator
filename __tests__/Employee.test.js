b1970a317f1af7a779c57e4c2592bcd4a0f3763c
Leah Russell <leahsigridrussell@gmail.com>
2021-04-10 17:00:42 -0700

    Adding employee object and tests

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,34 @@
+const { test, expect } = require('@jest/globals');
+const Employee = require('../lib/Employee.js');
+
+test('creates an Employee object', () => {
+    const employee = new Employee('Jill', 1, 'jill@test.com');
+
+    expect(employee.name).toBe('Jill');
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toBe('jill@test.com');
+});
+
+test('Get employee name', () => {
+    const employee = new Employee('Jill', 1, 'jill@test.com');
+
+    expect(employee.getName()).toHaveProperty('name');
+});
+
+test('Get employee id', () => {
+    const employee = new Employee('Jill', 1, 'jill@test.com');
+
+    expect(employee.getId()).toHaveProperty('id');
+});
+
+test('Get employee email', () => {
+    const employee = new Employee('Jill', 1, 'jill@test.com');
+
+    expect(employee.getEmail()).toHaveProperty('email');
+});
+
+test('Get employee role', () => {
+    const employee = new Employee('Jill', 1, 'jill@test.com');
+
+    expect(employee.getRole()).toBe('Employee');
+});
